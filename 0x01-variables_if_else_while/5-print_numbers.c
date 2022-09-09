e72c4adff5a8e7073cd9c1ad7d591a2a0bf6ac3c
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-09 20:00:31 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  Program entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
