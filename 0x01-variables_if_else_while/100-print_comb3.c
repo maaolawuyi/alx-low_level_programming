ff7d5d3641d1b9c3d50d0edf800de3ddd5c52b40
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-11 10:50:33 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program enrty point
+ *
+ * Return: 0 ALways (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			if (b != a)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
