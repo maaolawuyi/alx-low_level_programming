ce46304f216c7f578e1b863311ae430bb0c68dc0
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-11 12:28:55 +0000

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*main - Entry point
-*Return: Always 1 (Success)
-*/
+ * main - Progra entry point
+ *
+ * Return:Always 1 (Success)
+ */
+
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-				\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\n - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
