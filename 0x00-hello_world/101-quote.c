4663a81835c0e7d286600a2ed6166a781a7258ae
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-12 09:25:24 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 /**
@@ -6,9 +7,9 @@
  *
  * Return:Always 1 (Success)
  */
-
 int main(void)
 {
+
 	write(2,
 
 "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
