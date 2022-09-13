fa27b23b8a9238d5c1c1f84d0b8b4d4615d05918
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-13 12:50:55 +0000

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
@@ -7,12 +6,12 @@
  *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-
 	write(2,
 
-"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
