4edae550b95660823e58e390c24b86f3c2a00d57
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-15 11:54:10 +0000

    header file main.h added

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAAOLAWUYI_H
+#define MAAOLAWUYI_H
+
+/*
+ * File: maaolawuyi.h
+ * Disc: Header file containing declaration of functions
+ * use in the 0x04-More function_more_nested_loop directory.
+ *
+ *
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
+#endif
+
+
