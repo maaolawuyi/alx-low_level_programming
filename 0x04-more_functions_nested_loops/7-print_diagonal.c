a41940ba4ade59e5e34b08d2813397288ebd59b0
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-16 11:29:07 +0000

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,11 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 				_putchar(' ');
+
 			_putchar(92);
 			_putchar('\n');
 		}
