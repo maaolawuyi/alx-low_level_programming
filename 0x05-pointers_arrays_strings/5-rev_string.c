01ab8a711404b1e8053919f2debc348ce5cdc176
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-19 22:22:13 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string a function the reverse string
+ * follow by new line
+ * @s: input character
+ * Return: Nothing.
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char sw;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len--)
+	{
+		sw = s[i];
+		s[i++] = s[i];
+		s[len] = sw;
+	}
+}
