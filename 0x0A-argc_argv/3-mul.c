999e981849205d28b5743f383e4ee4713a5bbd3e
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-30 12:10:17 +0000

    task 3 to 5

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiply two numbers
+ * @argc: argument couter
+ * @argv: argument valus
+ * Return: always 0 success
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+
+	if (argc != 3)
+	{
+		printf("Error!");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
