438910b4d2622268c92b2a5f059dfff2c47f24fc
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-09-30 10:32:39 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -  a program that prints the number of arguments passed into it.
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: Always 1 success
+ */
+int main(int argc, char *argv[])
+{
+	if (agrv[0])
+		printf("%d\n", agrc - 1);
+
+	return (0);
+}
