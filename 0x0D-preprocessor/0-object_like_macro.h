17beddb6bb1b27469d2e20f73abf55356738773f
maaolawuyi@gmail.com <maaolawuyi@gmail.com>
2022-10-10 17:27:06 +0000

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _OBJECT_LIKE_MACRO_H_
-#define _OBJECT_LIKE_MACRO_H_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 #define SIZE 1024
-#endif /* _OBJECT_LIKE_MACRO_H_ */
+#endif /* OBJECT_LIKE_MACRO_H */
