ce0dca20a0b9e0d051e04ecc8bfea28d66b949c8
mojeed olawuyi <maaolawuyi@gmail.com>
2022-10-20 10:58:10 -0700

    task completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  main program entry
+ * Return: Nothing.
+ */
+void dmain(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
