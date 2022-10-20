506855e2f4f2189a4cc775c98d17ec406e5774e6
mojeed olawuyi <maaolawuyi@gmail.com>
2022-10-20 14:08:01 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+void main_constructor(void) __attribute__((constructor));
 /**
  * main -  main program entry
  * Return: Nothing.
  */
-void dmain(void)
+void main_constructor(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
