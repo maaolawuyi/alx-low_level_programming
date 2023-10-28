f400360d91ea05a83b50401354e387f0d6c2bc39
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-10-28 08:12:02 -0700

    file added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - print exactly "and that piece of art is useful"
  * - Dora korpar, 2015-10-19
  * follow by a newline, to the standard error
- * Retuen: 0 Always
+ * Return: 0 Always
  */ 
 int main(void)
 {
