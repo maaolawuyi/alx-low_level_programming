49ce8713752a22675ac23edf01a5540c918037a4
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-10-28 08:09:31 -0700

    file added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - print exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19
- * Reteun: 0 Always
- */
+ * - Dora korpar, 2015-10-19
+ * follow by a newline, to the standard error
+ * Retuen: 0 Always
+ */ 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
