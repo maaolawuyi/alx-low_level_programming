1a106201dd154c5b4e1f6bd3347a9f90a6fd483d
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-10-31 13:39:57 -0700

    file added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - adds two integers and returns the result
  * @a: int to be added
- * @b: int to be added 
+ * @b: int to be added
  * Return: sum of a and b
  */
 int add(int a, int b)
