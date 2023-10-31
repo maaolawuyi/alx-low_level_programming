64ee63834aa1bf1d025e75a756f47e6992cde748
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-10-31 08:35:41 -0700

    file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
