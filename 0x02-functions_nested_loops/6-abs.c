792a2f041555eb659f0896066d556f7de760dbe9
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-10-31 09:50:23 -0700

    file added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value of an integer
+ * @n: the int to compute
+ * Return: the absolute value of int
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
