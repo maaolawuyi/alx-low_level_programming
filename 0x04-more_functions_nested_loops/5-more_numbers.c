deea4788e3e000af7abbf1ae55ad3a56e2e9ff57
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-11-02 16:05:32 -0700

    file added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "mail.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * follow by newline
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
