3eaba975ba1eb17d4048dad8c643f1b4c01e31a4
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-11-17 15:03:04 -0800

    files added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+int _atoi(char *s);
+
 /**
  * main - adds two positive number
  * @ac: arguments counter
@@ -28,7 +30,7 @@ int main(int ac, char *av[])
 
 	for (k = 1; k < ac; k++)
 	{
-		num = atoi(av[k]);
+		num = _atoi(av[k]);
 		if (num >= 0)
 		{
 			sum += num;
@@ -37,4 +39,50 @@ int main(int ac, char *av[])
 
 	printf("%d\n", sum);
 	return (0);
+}
+
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
