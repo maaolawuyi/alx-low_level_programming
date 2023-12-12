6dd885a4ff51222898115cf9c4cc9ee5c59d3295
Mojeed Olawuyi <maaolawuyi@gmail.com>
2023-12-12 10:07:31 +0000

    file added

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *p = NULL;
-	listint_t *n = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
 	while (*head)
 	{
-		n = (*head)->n;
-		(*head)->n = p;
-		p = *head;
-		*head = n;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	*head = p;
+	*head = prev;
 
 	return (*head);
 }
